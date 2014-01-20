68f16f3b48130266fab106f30ecf77e3efb49517
TrophyChase <jordan@snydercga.com>
2014-01-19 21:12:33 -0500

    added solution for S2 2010

---
+++ 2010_S2.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<map>
+#include<string>
+using namespace std;
+
+int main()
+{
+    int num_char;
+    int length=1;
+    char character;
+    string code;
+    string sequence;
+    map <string,char> huffman;
+
+    cin >> num_char;
+
+    //get the characters and their associated codes
+    for (int x=1;x<=num_char; x++)
+    {
+        cin >> character;
+        cin >> code;
+        huffman[code]=character;
+    }
+    //get the sequence
+    cin >> sequence;
+
+    while(sequence.length()>0)
+    {   //if the substring maps to a character, output the character, erase the substring, and set length to 1
+        if (huffman.find(sequence.substr(0,length))!= huffman.end())
+        {
+             cout<<huffman[sequence.substr(0,length)];
+             sequence.erase(0,length);
+             length=1;
+        }
+        //otherwise, increase the size of the substring to check
+        else
+            length++;
+    }
+
+}
